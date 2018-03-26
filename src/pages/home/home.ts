6a19c3c397cd8bba3276512f8d0b23b311f03b15
Eduardo Morais <emorais1375@gmail.com>
2018-03-26 11:51:33 -0400

    Adicina link as imagens

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,17 +16,17 @@ export class HomePage {
     {
       title: "Welcome to the Docs!",
       description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
-      image: "assets/imgs/event01.jpg",
+      image: "http://www.cultura.am.gov.br/conteudo/uploads/2018/03/exposi%C3%A7%C3%A3o-fotografica-carnaval-1.jpg",
     },
     {
       title: "What is Ionic?",
       description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
-      image: "assets/imgs/event02.jpg",
+      image: "http://www.cultura.am.gov.br/conteudo/uploads/2014/04/FOTO-2.jpg",
     },
     {
       title: "What is Ionic Cloud?",
       description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
-      image: "assets/imgs/event03.jpg",
+      image: "http://www.cultura.am.gov.br/conteudo/uploads/2018/03/exposi%C3%A7%C3%A3o-fotografica-carnaval-1.jpg",
     }
   ];
   public list_event = new Array<any>();
